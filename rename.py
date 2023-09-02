4515d444bf7b9bd7af3546dbfe9ebcfced637075
ManiacBoy777 <rkm0722@gmail.com>
2023-09-01 22:25:19 -0700

    Update rename.py
    removed termux filepaths and replaced with $HOME variable

--- rename.py
+++ rename.py
@@ -1,6 +1,6 @@
 newName = input("[+] enter the new name : ")
 newName = '"'+newName+'"'
-file  = open('/data/data/com.termux/files/home/.zshrc','r')
+file  = open('$HOME/.zshrc','r')
 allLines = []
 i = 0;pos= 0
 for line in file.readlines():
@@ -10,7 +10,7 @@ for line in file.readlines():
     i+=1
 allLines[pos]="TNAME="+newName+"\n"
 file.close()
-file = open('/data/data/com.termux/files/home/.zshrc','w')
+file = open('$HOME/.zshrc','w')
 file.writelines(allLines)
 file.close()
 print("Updated sucessfully restart to see changes")
