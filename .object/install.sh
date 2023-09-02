c0b19d2c980d0bf525f42e211fd5b0a258bc686c
ManiacBoy777 <rkm0722@gmail.com>
2023-09-02 11:25:21 -0700

    Create install.sh

---
+++ .object/install.sh
@@ -0,0 +1,4 @@
+apt update -y
+apt upgrade -y
+apt install git figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y
+bash $HOME/T-Header/t-header.sh
