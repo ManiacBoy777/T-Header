43bc8204642ce022ab50c0fa91ee84ed9f6c91fe
ManiacBoy777 <rkm0722@gmail.com>
2023-09-02 12:36:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-su - root
-cd /root/
-apt update -y
-apt upgrade -y
-apt install figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y
-rm -f /etc/pam.d/chsh
-cp $HOME/T-Header/chsh /etc/pam.d/chsh
+su - root &
+cd /root/ &
+apt update -y &&
+apt upgrade -y &&
+apt install figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y &&
+rm -f /etc/pam.d/chsh &&
+cp $HOME/T-Header/chsh /etc/pam.d/chsh &&
 bash $HOME/T-Header/t-header.sh
