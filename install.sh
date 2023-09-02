281d773ca04f9e210c9da91a5d0bec14e6ac060b
ManiacBoy777 <rkm0722@gmail.com>
2023-09-02 11:39:09 -0700

    Update install.sh
    removed install git, will be added to readme for install command

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update -y
 apt upgrade -y
-apt install git figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y
+apt install figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y
 rm -f /etc/pam.d/chsh
 cp $HOME/T-Header/chsh /etc/pam.d/chsh
 bash $HOME/T-Header/t-header.sh
