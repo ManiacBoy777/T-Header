e3973a52585ca0bdec4db2525f71b9bc7eb1fc39
ManiacBoy777 <rkm0722@gmail.com>
2023-09-03 11:38:47 -0700

    Delete .object/install.sh

--- .object/install.sh
+++
@@ -1,4 +0,0 @@
-apt update -y
-apt upgrade -y
-apt install git figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y
-bash $HOME/T-Header/t-header.sh
