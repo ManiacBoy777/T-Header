fa557c1ff51e731de3e3a65dc706d70a1f7f913b
ManiacBoy777 <rkm0722@gmail.com>
2023-09-04 08:03:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,4 @@
 echo "";
 echo -e "\e[1;34m[ ] \e[32mInstalling packages \e[0m";
 echo "";
-(su - root &
-cd $HOME/ &
-apt update -y &&
-apt upgrade -y &&
-apt install figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y &&
-rm -f /etc/pam.d/chsh &&
-cp $HOME/T-Header/chsh /etc/pam.d/chsh &&
-chmod +x $HOME/T-Header/uninstall.sh
-bash $HOME/T-Header/t-header.sh) &> /dev/null;
+(su - root & cd $HOME/ & apt update -y && apt upgrade -y && apt install figlet pv binutils coreutils wget git zsh procps gawk exa neofetch lolcat libncurses5-dev libncursesw5-dev -y && rm -f /etc/pam.d/chsh && cp $HOME/T-Header/chsh /etc/pam.d/chsh && chmod +x $HOME/T-Header/uninstall.sh && bash $HOME/T-Header/t-header.sh) &> /dev/null
