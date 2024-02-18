66cbae33d00e30956a54b6fdc48416572a8e1763
ManiacBoy777 <rkm0722@gmail.com>
2024-02-18 08:00:40 -0700

    Update t-header.sh
    add sudo function. adding sudo commands later

--- t-header.sh
+++ t-header.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+# Define a function that runs a command with sudo if possible and needed
+sudo_if_possible() {
+  # Check if sudo is available
+  if command -v sudo >/dev/null 2>&1; then
+    # Check if the user is not root
+    if [ "$EUID" -ne 0 ]; then
+      # Run the command with sudo
+      sudo "$@"
+    else
+      # Run the command without sudo
+      "$@"
+    fi
+  else
+    # Run the command without sudo
+    "$@"
+  fi
+}
+
 if [[ "$1" == "--remove" ]]; then
-	rm -rf $HOME/.oh-my-zsh $HOME/.plugins;
+ rm -rf $HOME/.oh-my-zsh $HOME/.plugins;
 	rm -rf $HOME/.bashrc;
  	rm -rf $HOME/.banner.sh;
   	rm -rf $HOME/.draw;
