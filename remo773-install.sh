7a94ba265c7ef6f2ee69674a8f405fd6c5d42870
ManiacBoy777 <rkm0722@gmail.com>
2025-06-19 22:19:28 -0700

    actually added the new script

---
+++ remo773-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Define a function that runs a command with sudo if possible and needed
+sudo_if_possible() {
+  # Check if sudo is available
+  if command -v sudo >/dev/null 2>&1; then
+    # Check if the user is not root
+    if [[ "$EUID" -ne 0 ]]; then
+      # Run the command with sudo
+      sudo "$@"
+    else
+      # Run the command without sudo
+      "$@"
+    fi
+  else
+    # Run the command without sudo
+    "$@"
+  fi
